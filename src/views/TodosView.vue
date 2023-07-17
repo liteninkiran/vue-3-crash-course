b26be5a33dfab2b4ee39f90f7ba75114d7a3862b
Kiran Anand <liteninkiran@hotmail.com>
2023-07-17 11:51:23 +0100

    Section 13 - Save to local storage

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -6,6 +6,16 @@
     import TodoItem from '../components/TodoItem.vue';
 
     const todoList = ref([]);
+    const fetchTodoList = () => {
+        const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
+        if (savedTodoList) {
+            todoList.value = savedTodoList;
+        }
+    };
+    fetchTodoList();
+    const setTodoListLocalStorage = () => {
+        localStorage.setItem('todoList', JSON.stringify(todoList.value));
+    };
     const createTodo = (todo) => {
         todoList.value.push({
             id: uid(),
@@ -13,18 +23,23 @@
             isCompleted: null,
             isEditing: null,
         });
+        setTodoListLocalStorage();
     };
     const toggleTodoComplete = (index) => {
         todoList.value[index].isCompleted = !todoList.value[index].isCompleted;
+        setTodoListLocalStorage();
     };
     const toggleEditTodo = (index) => {
         todoList.value[index].isEditing = !todoList.value[index].isEditing;
+        setTodoListLocalStorage();
     };
     const updateTodo = (value, index) => {
         todoList.value[index].todo = value;
+        setTodoListLocalStorage();
     };
     const deleteTodo = (id) => {
         todoList.value = todoList.value.filter((todo) => todo.id !== id);
+        setTodoListLocalStorage();
     };
 </script>
 
